968bfe2efe40086ecb7de1a2d650b3357efc32c5
Sergiu Lucaci <sergius.lucaci@gmail.com>
2021-01-14 16:07:28 +0200

    configure eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,18 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+  parser: '@typescript-eslint/parser',
+  extends: ['airbnb'],
+  plugins: ['@typescript-eslint', 'react', 'jsx-a11y', 'import', 'prettier', 'react-hooks'],
+  rules: {
+    'import/no-unresolved': 0,
+    'react/jsx-filename-extension': [1, {
+      extensions: [
+        '.js',
+        '.jsx',
+        '.ts',
+        '.tsx',
+      ],
+    }],
+    'import/extensions': 'off',
+  },
 };
